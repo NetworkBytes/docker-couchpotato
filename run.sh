8cd900d78b6c6b60efae9a468f10c927fe529f6d
John Bencic <john@bencic.net>
2016-04-19 21:44:49 +1000

    added link to transmission

--- run.sh
+++ run.sh
@@ -5,13 +5,14 @@ IMGNAME=networkbytes/$NAME:v$VERSION
 docker rm $NAME
 
 docker run \
+--link transmission:transmission \
 -v /etc/localtime:/etc/localtime:ro \
 -v /mnt/share/Media/Transmission/Drop:/Drop \
 -v /mnt/share/Media/Movies.unsorted:/Movies.unsorted \
 -v /mnt/share/Media/Movies:/Movies \
 -v $PWD/config:/config \
 -p 5050:5050 \
--d \
+-it \
 --privileged=true \
 --name $NAME \
 $IMGNAME
